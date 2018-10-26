3e8122f49f24cdfac0c9abfcabfc5dd5a573b466
Katie Ashlin <akka726@outlook.com>
2018-10-26 02:20:49 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,8 @@
 def countdown(to_midnight)
   to_midnight = 10
   while to_midnight > 0
-   seconds_to_midnight -= 1
   puts "#{to_midnight} SECOND(S)!"
+  to_midnight -= 1
   "HAPPY NEW YEAR!"
 end
 end
